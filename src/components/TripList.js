8493ff9d13f2b0768183d93026f43459602e82a3
shivang <shivangmathur101@gmail.com>
2022-03-11 00:46:38 +0530

    outputed list

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import "./TripList.css";
 export default function TripList() {
   const [trips, setTrips] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((res) => res.json())
+      .then((data) => setTrips(data));
+  }, []);
   console.log(trips);
 
-  fetch("http://localhost:3000/trips")
-    .then((response) => response.json())
-    .then((json) => setTrips);
-
   return (
-    <div>
+    <div className="trip-list">
       <h2>TripList</h2>
+      <ul>
+        {trips.map((trip) => {
+          return (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
